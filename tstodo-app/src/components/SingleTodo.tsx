f5ff164494ce570f218d15cdc6e3206ce8867a45
nietovaca <nietovaca@gmail.com>
2022-03-23 11:30:55 -0600

    C and R done/installing mui icons

---
+++ tstodo-app/src/components/SingleTodo.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CheckIcon from '@mui/icons-material/Check';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+const SingleTodo = () => {
+  return(
+    <div>
+    </div>
+  )
+}
+
+export default SingleTodo
